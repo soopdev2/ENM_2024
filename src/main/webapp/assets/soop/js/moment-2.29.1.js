30006f971adeee6c676d04fbb85741b96ea02dc7
Aldo <Aldo@host.docker.internal>
2024-05-09 10:16:43 +0200

    Revert "more fix for code quality. "
    This reverts commit 6d8ca38e411cf92a3fd8889bbbf8101f16735a78.

--- src/main/webapp/assets/soop/js/moment-2.29.1.js
+++ src/main/webapp/assets/soop/js/moment-2.29.1.js
@@ -4,12 +4,11 @@
 //! license : MIT
 //! momentjs.com
 
-; (function (global, factory) {
+;(function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-        typeof define === 'function' && define.amd ? define(factory) :
-            global.moment = factory()
-}(this, (function () {
-    'use strict';
+    typeof define === 'function' && define.amd ? define(factory) :
+    global.moment = factory()
+}(this, (function () { 'use strict';
 
     var hookCallback;
 
@@ -314,10 +313,10 @@
                 }
                 warn(
                     msg +
-                    '\nArguments: ' +
-                    Array.prototype.slice.call(args).join('') +
-                    '\n' +
-                    new Error().stack
+                        '\nArguments: ' +
+                        Array.prototype.slice.call(args).join('') +
+                        '\n' +
+                        new Error().stack
                 );
                 firstTime = false;
             }
@@ -365,8 +364,8 @@
         // TODO: Remove "ordinalParse" fallback in next major release.
         this._dayOfMonthOrdinalParseLenient = new RegExp(
             (this._dayOfMonthOrdinalParse.source || this._ordinalParse.source) +
-            '|' +
-            /\d{1,2}/.source
+                '|' +
+                /\d{1,2}/.source
         );
     }
 
@@ -791,8 +790,8 @@
         regexes[token] = isFunction(regex)
             ? regex
             : function (isStrict, localeData) {
-                return isStrict && strictRegex ? strictRegex : regex;
-            };
+                  return isStrict && strictRegex ? strictRegex : regex;
+              };
     }
 
     function getParseRegexForToken(token, config) {
@@ -949,8 +948,8 @@
     // LOCALES
 
     var defaultLocaleMonths = 'January_February_March_April_May_June_July_August_September_October_November_December'.split(
-        '_'
-    ),
+            '_'
+        ),
         defaultLocaleMonthsShort = 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split(
             '_'
         ),
@@ -967,10 +966,10 @@
         return isArray(this._months)
             ? this._months[m.month()]
             : this._months[
-            (this._months.isFormat || MONTHS_IN_FORMAT).test(format)
-                ? 'format'
-                : 'standalone'
-            ][m.month()];
+                  (this._months.isFormat || MONTHS_IN_FORMAT).test(format)
+                      ? 'format'
+                      : 'standalone'
+              ][m.month()];
     }
 
     function localeMonthsShort(m, format) {
@@ -982,8 +981,8 @@
         return isArray(this._monthsShort)
             ? this._monthsShort[m.month()]
             : this._monthsShort[
-            MONTHS_IN_FORMAT.test(format) ? 'format' : 'standalone'
-            ][m.month()];
+                  MONTHS_IN_FORMAT.test(format) ? 'format' : 'standalone'
+              ][m.month()];
     }
 
     function handleStrictParse(monthName, format, strict) {
@@ -1054,18 +1053,18 @@
             mom = createUTC([2000, i]);
             if (strict && !this._longMonthsParse[i]) {
                 this._longMonthsParse[i] = new RegExp(
-                    '^' + this.months(mom, '').replace(/\./g, '') + '$',
+                    '^' + this.months(mom, '').replace('.', '') + '$',
                     'i'
                 );
                 this._shortMonthsParse[i] = new RegExp(
-                    '^' + this.monthsShort(mom, '').replace(/\./g, '') + '$',
+                    '^' + this.monthsShort(mom, '').replace('.', '') + '$',
                     'i'
                 );
             }
             if (!strict && !this._monthsParse[i]) {
                 regex =
                     '^' + this.months(mom, '') + '|^' + this.monthsShort(mom, '');
-                this._monthsParse[i] = new RegExp(regex.replace(/\./g, ''), 'i');
+                this._monthsParse[i] = new RegExp(regex.replace('.', ''), 'i');
             }
             // test the regex
             if (
@@ -1528,8 +1527,8 @@
     }
 
     var defaultLocaleWeekdays = 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split(
-        '_'
-    ),
+            '_'
+        ),
         defaultLocaleWeekdaysShort = 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
         defaultLocaleWeekdaysMin = 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
         defaultWeekdaysRegex = matchWord,
@@ -1540,31 +1539,31 @@
         var weekdays = isArray(this._weekdays)
             ? this._weekdays
             : this._weekdays[
-            m && m !== true && this._weekdays.isFormat.test(format)
-                ? 'format'
-                : 'standalone'
-            ];
+                  m && m !== true && this._weekdays.isFormat.test(format)
+                      ? 'format'
+                      : 'standalone'
+              ];
         return m === true
             ? shiftWeekdays(weekdays, this._week.dow)
             : m
-                ? weekdays[m.day()]
-                : weekdays;
+            ? weekdays[m.day()]
+            : weekdays;
     }
 
     function localeWeekdaysShort(m) {
         return m === true
             ? shiftWeekdays(this._weekdaysShort, this._week.dow)
             : m
-                ? this._weekdaysShort[m.day()]
-                : this._weekdaysShort;
+            ? this._weekdaysShort[m.day()]
+            : this._weekdaysShort;
     }
 
     function localeWeekdaysMin(m) {
         return m === true
             ? shiftWeekdays(this._weekdaysMin, this._week.dow)
             : m
-                ? this._weekdaysMin[m.day()]
-                : this._weekdaysMin;
+            ? this._weekdaysMin[m.day()]
+            : this._weekdaysMin;
     }
 
     function handleStrictParse$1(weekdayName, format, strict) {
@@ -1660,15 +1659,15 @@
             mom = createUTC([2000, 1]).day(i);
             if (strict && !this._fullWeekdaysParse[i]) {
                 this._fullWeekdaysParse[i] = new RegExp(
-                    '^' + this.weekdays(mom, '').replace(/\./g, '\\.?') + '$',
+                    '^' + this.weekdays(mom, '').replace('.', '\\.?') + '$',
                     'i'
                 );
                 this._shortWeekdaysParse[i] = new RegExp(
-                    '^' + this.weekdaysShort(mom, '').replace(/\./g, '\\.?') + '$',
+                    '^' + this.weekdaysShort(mom, '').replace('.', '\\.?') + '$',
                     'i'
                 );
                 this._minWeekdaysParse[i] = new RegExp(
-                    '^' + this.weekdaysMin(mom, '').replace(/\./g, '\\.?') + '$',
+                    '^' + this.weekdaysMin(mom, '').replace('.', '\\.?') + '$',
                     'i'
                 );
             }
@@ -1680,7 +1679,7 @@
                     this.weekdaysShort(mom, '') +
                     '|^' +
                     this.weekdaysMin(mom, '');
-                this._weekdaysParse[i] = new RegExp(regex.replace(/\./g, ''), 'i');
+                this._weekdaysParse[i] = new RegExp(regex.replace('.', ''), 'i');
             }
             // test the regex
             if (
@@ -2138,9 +2137,9 @@
                 deprecateSimple(
                     'defineLocaleOverride',
                     'use moment.updateLocale(localeName, config) to change ' +
-                    'an existing locale. moment.defineLocale(localeName, ' +
-                    'config) should only be used for creating a new locale ' +
-                    'See http://momentjs.com/guides/#/warnings/define-locale/ for more info.'
+                        'an existing locale. moment.defineLocale(localeName, ' +
+                        'config) should only be used for creating a new locale ' +
+                        'See http://momentjs.com/guides/#/warnings/define-locale/ for more info.'
                 );
                 parentConfig = locales[name]._config;
             } else if (config.parentLocale != null) {
@@ -2265,21 +2264,21 @@
                 a[MONTH] < 0 || a[MONTH] > 11
                     ? MONTH
                     : a[DATE] < 1 || a[DATE] > daysInMonth(a[YEAR], a[MONTH])
-                        ? DATE
-                        : a[HOUR] < 0 ||
-                            a[HOUR] > 24 ||
-                            (a[HOUR] === 24 &&
-                                (a[MINUTE] !== 0 ||
-                                    a[SECOND] !== 0 ||
-                                    a[MILLISECOND] !== 0))
-                            ? HOUR
-                            : a[MINUTE] < 0 || a[MINUTE] > 59
-                                ? MINUTE
-                                : a[SECOND] < 0 || a[SECOND] > 59
-                                    ? SECOND
-                                    : a[MILLISECOND] < 0 || a[MILLISECOND] > 999
-                                        ? MILLISECOND
-                                        : -1;
+                    ? DATE
+                    : a[HOUR] < 0 ||
+                      a[HOUR] > 24 ||
+                      (a[HOUR] === 24 &&
+                          (a[MINUTE] !== 0 ||
+                              a[SECOND] !== 0 ||
+                              a[MILLISECOND] !== 0))
+                    ? HOUR
+                    : a[MINUTE] < 0 || a[MINUTE] > 59
+                    ? MINUTE
+                    : a[SECOND] < 0 || a[SECOND] > 59
+                    ? SECOND
+                    : a[MILLISECOND] < 0 || a[MILLISECOND] > 999
+                    ? MILLISECOND
+                    : -1;
 
             if (
                 getParsingFlags(m)._overflowDayOfYear &&
@@ -2540,8 +2539,8 @@
 
     hooks.createFromInputFallback = deprecate(
         'value provided is not in a recognized RFC2822 or ISO format. moment construction falls back to js Date(), ' +
-        'which is not reliable across all browsers and versions. Non RFC2822/ISO date formats are ' +
-        'discouraged. Please refer to http://momentjs.com/guides/#/warnings/js-date/ for more info.',
+            'which is not reliable across all browsers and versions. Non RFC2822/ISO date formats are ' +
+            'discouraged. Please refer to http://momentjs.com/guides/#/warnings/js-date/ for more info.',
         function (config) {
             config._d = new Date(config._i + (config._useUTC ? ' UTC' : ''));
         }
@@ -2726,10 +2725,10 @@
     }
 
     // constant that refers to the ISO standard
-    hooks.ISO_8601 = function () { };
+    hooks.ISO_8601 = function () {};
 
     // constant that refers to the RFC 2822 form
-    hooks.RFC_2822 = function () { };
+    hooks.RFC_2822 = function () {};
 
     // date from string and format string
     function configFromStringAndFormat(config) {
@@ -3027,16 +3026,16 @@
     }
 
     var prototypeMin = deprecate(
-        'moment().min is deprecated, use moment.max instead. http://momentjs.com/guides/#/warnings/min-max/',
-        function () {
-            var other = createLocal.apply(null, arguments);
-            if (this.isValid() && other.isValid()) {
-                return other < this ? this : other;
-            } else {
-                return createInvalid();
+            'moment().min is deprecated, use moment.max instead. http://momentjs.com/guides/#/warnings/min-max/',
+            function () {
+                var other = createLocal.apply(null, arguments);
+                if (this.isValid() && other.isValid()) {
+                    return other < this ? this : other;
+                } else {
+                    return createInvalid();
+                }
             }
-        }
-    ),
+        ),
         prototypeMax = deprecate(
             'moment().max is deprecated, use moment.min instead. http://momentjs.com/guides/#/warnings/min-max/',
             function () {
@@ -3285,7 +3284,7 @@
 
     // This function will be called whenever a moment is mutated.
     // It is intended to keep the offset in sync with the timezone.
-    hooks.updateOffset = function () { };
+    hooks.updateOffset = function () {};
 
     // MOMENTS
 
@@ -3568,11 +3567,11 @@
                 deprecateSimple(
                     name,
                     'moment().' +
-                    name +
-                    '(period, number) is deprecated. Please use moment().' +
-                    name +
-                    '(number, period). ' +
-                    'See http://momentjs.com/guides/#/warnings/add-inverted-param/ for more info.'
+                        name +
+                        '(period, number) is deprecated. Please use moment().' +
+                        name +
+                        '(number, period). ' +
+                        'See http://momentjs.com/guides/#/warnings/add-inverted-param/ for more info.'
                 );
                 tmp = val;
                 val = period;
@@ -3711,16 +3710,16 @@
         return diff < -6
             ? 'sameElse'
             : diff < -1
-                ? 'lastWeek'
-                : diff < 0
-                    ? 'lastDay'
-                    : diff < 1
-                        ? 'sameDay'
-                        : diff < 2
-                            ? 'nextDay'
-                            : diff < 7
-                                ? 'nextWeek'
-                                : 'sameElse';
+            ? 'lastWeek'
+            : diff < 0
+            ? 'lastDay'
+            : diff < 1
+            ? 'sameDay'
+            : diff < 2
+            ? 'nextDay'
+            : diff < 7
+            ? 'nextWeek'
+            : 'sameElse';
     }
 
     function calendar$1(time, formats) {
@@ -5147,12 +5146,12 @@
                     toInt((number % 100) / 10) === 1
                         ? 'th'
                         : b === 1
-                            ? 'st'
-                            : b === 2
-                                ? 'nd'
-                                : b === 3
-                                    ? 'rd'
-                                    : 'th';
+                        ? 'st'
+                        : b === 2
+                        ? 'nd'
+                        : b === 3
+                        ? 'rd'
+                        : 'th';
             return number + output;
         },
     });
